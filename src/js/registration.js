2f08436095da5a1f7861ce7ecdba12b0318f38b5
Caroline Nguyen <carolbnguyen@gmail.com>
2024-04-26 12:45:29 -0700

    Set up React app

--- src/js/registration.js
+++
@@ -1,6 +0,0 @@
-const signupForm = document.getElementById('signup-form');
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  window.location.href = '../pages/feed.html';
-  // TODO: mock API call
-});
