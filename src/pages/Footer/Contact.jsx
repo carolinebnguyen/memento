92438c17f22ef6d677b6c25ec70f8f2976fc7675
Caroline Nguyen <carolbnguyen@gmail.com>
2024-04-27 05:31:51 -0700

    Style Contact page and add form validation

--- src/pages/Footer/Contact.jsx
+++ src/pages/Footer/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Flex,
   Button,
@@ -7,22 +7,44 @@ import {
   Heading,
   Input,
   Stack,
-  InputGroup,
-  InputRightElement,
-  Box,
-  Divider,
-  AbsoluteCenter,
-  Link,
+  Textarea,
+  useToast,
+  Text,
 } from '@chakra-ui/react';
-import { FaEyeSlash, FaEye } from 'react-icons/fa';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 
 export default function Contact() {
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const toast = useToast();
 
-  const togglePasswordVisibility = () =>
-    setIsPasswordVisible(!isPasswordVisible);
+  const initialValues = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
+
+  const validationSchema = yup.object({
+    name: yup.string().required('Name is required'),
+    email: yup.string().email().required('Email is required'),
+    subject: yup.string().required('Subject is required'),
+    message: yup.string().required('Message is required'),
+  });
+
+  const onSubmit = (values, { setSubmitting, resetForm }) => {
+    resetForm(initialValues);
+    toast({
+      title: 'Message Sent',
+      description:
+        'Thank you! Your message has been sent. We will get back to you within 5 business days.',
+      status: 'success',
+      duration: 5000,
+      position: 'top',
+    });
+    setSubmitting(false);
+  };
 
   return (
     <>
@@ -32,52 +54,96 @@ export default function Contact() {
           <Stack padding={9} paddingTop={0} align="center">
             <Heading as="h1">Contact Us</Heading>
             <Stack w="100%">
-              <form>
-                <FormControl>
-                  <FormLabel fontSize="16px" fontWeight="500" mt={5}>
-                    Email Address
-                  </FormLabel>
-                  <Input type="email" id="email" isRequired />
-                  <FormLabel fontSize="16px" fontWeight="500" mt={5}>
-                    Password
-                  </FormLabel>
-                  <InputGroup>
-                    <Input
-                      type={isPasswordVisible ? 'text' : 'password'}
-                      id="password"
-                      isRequired
-                    />
-                    <InputRightElement>
-                      <Button
-                        onClick={togglePasswordVisibility}
-                        h="1.75rem"
-                        size="xs"
-                        colorScheme="whiteAlpha"
-                      >
-                        {isPasswordVisible ? (
-                          <FaEye size={22} color="#232323" />
-                        ) : (
-                          <FaEyeSlash size={22} color="#232323" />
-                        )}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                  <Button colorScheme="blue" type="submit" w="100%" mt={10}>
-                    Log in
-                  </Button>
-                </FormControl>
-              </form>
-              <Box position="relative" mt={5}>
-                <Divider />
-                <AbsoluteCenter bg="white" px="4">
-                  or
-                </AbsoluteCenter>
-              </Box>
-              <Link href="/signup">
-                <Button variant="outline" w="100%" mt={5}>
-                  Sign up
-                </Button>
-              </Link>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+                enableReinitialize
+              >
+                {({
+                  values,
+                  isSubmitting,
+                  resetForm,
+                  handleSubmit,
+                  errors,
+                  touched,
+                }) => (
+                  <Form>
+                    <FormControl isRequired>
+                      <FormLabel fontSize="16px" mt={5}>
+                        Name
+                      </FormLabel>
+                      <Field
+                        as={Input}
+                        type="text"
+                        name="name"
+                        id="name"
+                        isInvalid={errors.name && touched.name}
+                        mb={2}
+                      />
+                      <ErrorMessage name="name">
+                        {(msg) => <Text color="red">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel fontSize="16px" mt={5}>
+                        Email Address
+                      </FormLabel>
+                      <Field
+                        as={Input}
+                        type="email"
+                        name="email"
+                        id="email"
+                        isInvalid={errors.email && touched.email}
+                        mb={2}
+                      />
+                      <ErrorMessage name="email">
+                        {(msg) => <Text color="red">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel fontSize="16px" mt={5}>
+                        Subject
+                      </FormLabel>
+                      <Field
+                        as={Input}
+                        type="text"
+                        name="subject"
+                        id="subject"
+                        isInvalid={errors.subject && touched.subject}
+                        mb={2}
+                      />
+                      <ErrorMessage name="subject">
+                        {(msg) => <Text color="red">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel fontSize="16px" mt={5}>
+                        Message
+                      </FormLabel>
+                      <Field
+                        as={Textarea}
+                        name="message"
+                        id="message"
+                        isInvalid={errors.message && touched.message}
+                        mb={2}
+                      />
+                      <ErrorMessage name="message">
+                        {(msg) => <Text color="red">{msg}</Text>}
+                      </ErrorMessage>
+                    </FormControl>
+                    <Button
+                      colorScheme="blue"
+                      type="submit"
+                      w="100%"
+                      mt={5}
+                      isLoading={isSubmitting}
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
             </Stack>
           </Stack>
         </Stack>
