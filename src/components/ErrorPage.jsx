1d702c0bc15649621b7bf545b15cd79d859d5a7a
Caroline Nguyen <carolbnguyen@gmail.com>
2024-04-29 02:42:52 -0700

    Update error page to return user to previous page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,9 +5,8 @@ import {
   Text,
   Flex,
   Image,
-  Grid,
-  GridItem,
   Heading,
+  VStack,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import notFoundDog from '../assets/notFoundDog.jpg';
@@ -15,48 +14,38 @@ import notFoundDog from '../assets/notFoundDog.jpg';
 export default function ErrorPage() {
   const navigate = useNavigate();
 
+  const handleBackClick = () => {
+    if (window.history.length > 1) {
+      navigate(-1);
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
-    <Flex align="center" justify="center" h="100vh">
-      <Grid
-        templateColumns={{ md: 'repeat(2, 1fr)' }}
-        templateRows={{ base: 'repeat(2, 1fr)', md: 'none' }}
-        columnGap={20}
-      >
-        <GridItem w="100%" h={{ base: '50vh', md: '100vh' }}>
-          <Flex align="center" justify="center" h="100%">
-            <Box align="center" justify="center" p={5}>
-              <Heading as="h1" size="4xl">
-                404
-              </Heading>
-              <Text fontSize="5xl">Page Not Found</Text>
-              <Text fontSize="xl">
-                Oops! The page you are looking for was not found.
-              </Text>
-              <Button m={5} colorScheme="blue" onClick={() => navigate('/')}>
-                Back to Home
-              </Button>
-            </Box>
-          </Flex>
-        </GridItem>
-        <GridItem w="100%" h={{ base: '50vh', md: '100vh' }}>
-          <Flex
-            align="center"
-            justify="center"
-            h="100%"
-            direction="column"
-            p={5}
-          >
-            <Image
-              src={notFoundDog}
-              boxSize="500px"
-              objectFit="cover"
-              objectPosition="center"
-              alt="Pomeranian with Santa hat"
-            />
-            <Text as="i">Source: missyminzi</Text>
-          </Flex>
-        </GridItem>
-      </Grid>
+    <Flex align="center" justify="center" h="100vh" direction="column">
+      <Box align="center" justify="center" p={5}>
+        <Heading as="h1" size="4xl">
+          404
+        </Heading>
+        <Text fontSize="5xl">Page Not Found</Text>
+        <Text fontSize="xl">
+          Oops! The page you are looking for was not found.
+        </Text>
+        <Button m={5} colorScheme="blue" onClick={handleBackClick}>
+          Go Back
+        </Button>
+      </Box>
+      <VStack>
+        <Image
+          src={notFoundDog}
+          boxSize="500px"
+          objectFit="cover"
+          objectPosition="center"
+          alt="Pomeranian with Santa hat"
+        />
+        <Text as="i">Source: missyminzi</Text>
+      </VStack>
     </Flex>
   );
 }
