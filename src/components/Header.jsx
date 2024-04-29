9b700c6a4f7e6ec8e6416083756241be01e990e2
Caroline Nguyen <carolbnguyen@gmail.com>
2024-04-28 23:11:18 -0700

    Add dropdown menu for user profile, settings, and log out options

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,61 @@
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import {
+  Flex,
+  Text,
+  Avatar,
+  Spacer,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Image,
+  Icon,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import { MdLogout, MdOutlineSettings } from 'react-icons/md';
+import caroline from '../assets/carolineAvatar.png';
+import { headerHeight } from '../utils/constants';
 
 export default function Header() {
+  const isWide = useBreakpointValue({ base: false, md: true });
+
   return (
     <Flex
       as="header"
       align="center"
       justify="space-between"
-      w="full"
+      w={isWide ? `calc(100vw - 300px)` : `full`}
       px={5}
       borderBottomWidth="1px"
       borderColor="blackAlpha.300"
-      h={14}
+      h={headerHeight}
     >
-      <Box>Header</Box>
+      <Spacer />
+      <Menu>
+        <MenuButton as={Avatar} src={caroline} size="sm" cursor="pointer" />
+        <MenuList>
+          <MenuItem>
+            <Image
+              boxSize="3rem"
+              borderRadius="full"
+              src={caroline}
+              alt="User profile picture"
+              mr={2}
+            />
+            <Text>My Profile</Text>
+          </MenuItem>
+          <MenuDivider />
+          <MenuItem>
+            <Icon as={MdOutlineSettings} boxSize={5} mr={3} />
+            Settings
+          </MenuItem>
+          <MenuItem>
+            <Icon as={MdLogout} boxSize={5} mr={3} />
+            Log Out
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </Flex>
   );
 }
