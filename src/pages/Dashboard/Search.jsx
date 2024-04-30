44a5bd4a62f0cf9b97ed637f2f7650be9baa44fc
Caroline Nguyen <carolbnguyen@gmail.com>
2024-04-29 21:42:12 -0700

    Create Search page

--- src/pages/Dashboard/Search.jsx
+++ src/pages/Dashboard/Search.jsx
@@ -1,6 +1,46 @@
-import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Flex,
+  Input,
+  Button,
+  HStack,
+  InputGroup,
+  InputLeftElement,
+  Icon,
+} from '@chakra-ui/react';
+import { MdOutlineSearch } from 'react-icons/md';
 
 export default function Search() {
-  return <Flex>Search</Flex>;
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    console.log(`Searched ${query}`);
+  };
+
+  const isSearchDisabled = query.trim() === '';
+
+  return (
+    <Flex align="center" justify="center" direction="column">
+      <HStack>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <Icon as={MdOutlineSearch} color="gray.300" boxSize={22} />
+          </InputLeftElement>
+          <Input
+            type="text"
+            placeholder="Search Memento"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </InputGroup>
+        <Button
+          colorScheme="blue"
+          onClick={handleSearch}
+          isDisabled={isSearchDisabled}
+        >
+          Search
+        </Button>
+      </HStack>
+    </Flex>
+  );
 }
