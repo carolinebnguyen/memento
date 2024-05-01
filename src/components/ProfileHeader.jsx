92494b276c788140d1da38ccabd3bef3fd63b479
Caroline Nguyen <carolbnguyen@gmail.com>
2024-05-01 14:41:33 -0700

    Fix styling of posts, followers, and following

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function ProfileHeader({ username, name, picture }) {
   return (
-    <Flex mb={4}>
+    <Flex mb={2}>
       <Avatar size="xl" mr={5} src={picture} />
       <Stack direction="column">
         <Stack direction="row" gap={5}>
@@ -19,15 +19,42 @@ export default function ProfileHeader({ username, name, picture }) {
         </Stack>
 
         <Stack direction="row" gap={10}>
-          <Text as="b" fontSize="sm">
-            4 posts
-          </Text>
-          <Text as="b" fontSize="sm">
-            1234 followers
-          </Text>
-          <Text as="b" fontSize="sm">
-            56 following
-          </Text>
+          <Stack
+            direction={{ base: 'column', sm: 'row' }}
+            gap={{ base: 0, sm: 1 }}
+            align="center"
+          >
+            <Text as="b" fontSize="sm">
+              4
+            </Text>
+            <Text fontSize="sm" fontWeight={500}>
+              posts
+            </Text>
+          </Stack>
+          <Stack
+            direction={{ base: 'column', sm: 'row' }}
+            gap={{ base: 0, sm: 1 }}
+            align="center"
+          >
+            <Text as="b" fontSize="sm">
+              1,234
+            </Text>
+            <Text fontSize="sm" fontWeight={500}>
+              followers
+            </Text>
+          </Stack>
+          <Stack
+            direction={{ base: 'column', sm: 'row' }}
+            gap={{ base: 0, sm: 1 }}
+            align="center"
+          >
+            <Text as="b" fontSize="sm">
+              56
+            </Text>
+            <Text fontSize="sm" fontWeight={500}>
+              following
+            </Text>
+          </Stack>
         </Stack>
 
         <Heading as="h2" size="xs" noOfLines={1}>
