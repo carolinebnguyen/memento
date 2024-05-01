ef976c77368daaed1149c97037893f738a87a6f1
Caroline Nguyen <carolbnguyen@gmail.com>
2024-05-01 15:55:15 -0700

    Create AllTab and add Tabs to Home

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { useTab, Button, useMultiStyleConfig, Icon } from '@chakra-ui/react';
 import { AiOutlinePicture, AiFillPicture } from 'react-icons/ai';
 import { IoMegaphoneOutline, IoMegaphoneSharp } from 'react-icons/io5';
+import { IoGridOutline, IoGrid } from 'react-icons/io5';
+
+const AllTab = React.forwardRef((props, ref) => {
+  const tabProps = useTab({ ...props, ref });
+  const isSelected = !!tabProps['aria-selected'];
+
+  const styles = useMultiStyleConfig('Tabs', tabProps);
+
+  return (
+    <Button __css={styles.tab} {...tabProps} role="tab">
+      <Icon as={isSelected ? IoGrid : IoGridOutline} mr="2" />
+      <span style={{ fontSize: '14px' }}>{tabProps.children}</span>
+    </Button>
+  );
+});
 
 const PostTab = React.forwardRef((props, ref) => {
   const tabProps = useTab({ ...props, ref });
@@ -31,4 +46,4 @@ const StatusTab = React.forwardRef((props, ref) => {
   );
 });
 
-export { PostTab, StatusTab };
+export { AllTab, PostTab, StatusTab };
