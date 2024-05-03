88e699f70df6a9c089d9542e70856ae0c93957cc
Caroline Nguyen <carolbnguyen@gmail.com>
2024-05-02 19:25:04 -0700

    Refactor post and status objects

--- src/utils/testData.js
+++ src/utils/testData.js
@@ -1,6 +1,85 @@
 import caroline from '../assets/placeholders/carolineAvatarClear.png';
 import snowie from '../assets/snowieAvatar.png';
+import brownPomeranian from '../assets/placeholders/brownPomeranian.jpg';
 import { NotificationType } from './utils';
+
+const carolibnPhoto1 = {
+  id: 2,
+  postedBy: 'carolibn',
+  imageSrc:
+    'https://m.media-amazon.com/images/I/51b0f7Fz4KL._AC_UF1000,1000_QL80_.jpg',
+  likes: [],
+  comments: [
+    {
+      id: 1,
+      user: 'snowie',
+      comment: 'Nice pic!',
+      postedAt: new Date('2024-04-29T02:00:00Z'),
+    },
+  ],
+  postedAt: new Date('2024-04-29T00:00:00Z'),
+};
+
+const carolibnPhoto2 = {
+  id: 4,
+  postedBy: 'carolibn',
+  imageSrc:
+    'https://ih1.redbubble.net/image.3787621272.3825/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+  likes: 0,
+  comments: [],
+  postedAt: new Date('2024-04-30T00:00:00Z'),
+};
+
+const carolibnStatus1 = {
+  id: 1,
+  postedBy: 'carolibn',
+  content: 'This is my first status',
+  likes: [],
+  comments: [
+    {
+      user: 'snowie',
+      id: 2,
+      comment: 'Hello there!',
+      postedAt: new Date('2024-03-30T00:00:00Z'),
+    },
+  ],
+  postedAt: new Date('2024-03-29T00:00:00Z'),
+};
+
+const carolibnStatus2 = {
+  id: 3,
+  postedBy: 'carolibn',
+  content: 'This is my second status! :)',
+  likes: [],
+  comments: [],
+  postedAt: new Date('2024-04-29T01:00:00Z'),
+};
+
+const carolibnStatus3 = {
+  id: 5,
+  postedBy: 'carolibn',
+  content:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus voluptate modi libero sequi rerum asperiores laboriosam, dolore inventore molestias tempore eveniet dignissimos? Beatae iusto quibusdam repudiandae blanditiis voluptatum odio?',
+  likes: [],
+  comments: [],
+  postedAt: new Date('2024-05-01T00:00:00Z'),
+};
+
+const snowiePhoto1 = {
+  id: 6,
+  postedBy: 'snowie',
+  imageSrc: brownPomeranian,
+  likes: [],
+  comments: [
+    {
+      id: 3,
+      user: 'carolibn',
+      comment: 'Cute! :)',
+      postedAt: new Date('2024-05-03T09:00:00Z'),
+    },
+  ],
+  postedAt: new Date('2024-05-03T00:00:00Z'),
+};
 
 const carolineProfile = {
   username: 'carolibn',
@@ -10,62 +89,8 @@ const carolineProfile = {
   bio: 'Developer \n Software Engineering Major',
   followersList: [],
   followingList: [],
-  photos: [
-    {
-      id: 2,
-      imageSrc:
-        'https://m.media-amazon.com/images/I/51b0f7Fz4KL._AC_UF1000,1000_QL80_.jpg',
-      likes: [],
-      comments: [
-        {
-          id: 1,
-          user: 'snowie',
-          comment: 'Nice pic!',
-          postedAt: new Date('2024-04-29T02:00:00Z'),
-        },
-      ],
-      postedAt: new Date('2024-04-29T00:00:00Z'),
-    },
-    {
-      id: 4,
-      imageSrc:
-        'https://ih1.redbubble.net/image.3787621272.3825/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-      likes: 0,
-      comments: [],
-      postedAt: new Date('2024-04-30T00:00:00Z'),
-    },
-  ],
-  statuses: [
-    {
-      id: 1,
-      content: 'This is my first status',
-      likes: [],
-      comments: [
-        {
-          user: 'snowie',
-          id: 2,
-          comment: 'Hello there!',
-          postedAt: new Date('2024-03-30T00:00:00Z'),
-        },
-      ],
-      postedAt: new Date('2024-03-29T00:00:00Z'),
-    },
-    {
-      id: 3,
-      content: 'This is my second status! :)',
-      likes: [],
-      comments: [],
-      postedAt: new Date('2024-04-29T01:00:00Z'),
-    },
-    {
-      id: 5,
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor doloribus voluptate modi libero sequi rerum asperiores laboriosam, dolore inventore molestias tempore eveniet dignissimos? Beatae iusto quibusdam repudiandae blanditiis voluptatum odio?',
-      likes: [],
-      comments: [],
-      postedAt: new Date('2024-05-01T00:00:00Z'),
-    },
-  ],
+  photos: [carolibnPhoto1, carolibnPhoto2],
+  statuses: [carolibnStatus1, carolibnStatus2, carolibnStatus3],
 };
 
 const snowieProfile = {
@@ -75,7 +100,7 @@ const snowieProfile = {
   bio: 'Pom',
   followersList: [carolineProfile],
   followingList: [carolineProfile],
-  photos: [],
+  photos: [snowiePhoto1],
   statuses: [],
 };
 
@@ -118,6 +143,22 @@ const notifications = [
   },
 ];
 
+const postIdToPostMap = {
+  1: carolibnStatus1,
+  2: carolibnPhoto1,
+  3: carolibnStatus2,
+  4: carolibnPhoto2,
+  5: carolibnStatus3,
+  6: snowiePhoto1,
+};
+
+const getPost = (postId) => {
+  if (!(postId in postIdToPostMap)) {
+    return '';
+  }
+  return postIdToPostMap[postId];
+};
+
 const usernameToProfileMap = {
   carolibn: carolineProfile,
   snowie: snowieProfile,
@@ -134,6 +175,8 @@ export {
   carolineProfile,
   snowieProfile,
   notifications,
+  postIdToPostMap,
+  getPost,
   usernameToProfileMap,
   getProfile,
 };
