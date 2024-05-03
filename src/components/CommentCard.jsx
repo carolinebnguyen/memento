e9f458f1a7d3a0e82ed2c8c4c469244952169904
Caroline Nguyen <carolbnguyen@gmail.com>
2024-05-02 21:05:04 -0700

    Create ConfirmationModal and add delete comment confirmation

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Flex,
   Avatar,
@@ -12,18 +12,24 @@ import {
   MenuList,
   MenuItem,
   IconButton,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
 import { FaEllipsis } from 'react-icons/fa6';
 import { getProfile } from '../utils/testData';
 import { formatDate, formatDateDistanceToNow } from '../utils/utils';
 import { useNavigate } from 'react-router-dom';
+import styles from '../components/BottomNav/BottomNav.module.css';
+import ConfirmationModal from './ConfirmationModal';
 
 export default function CommentCard({ poster, comment }) {
   const { user, content, postedAt } = comment;
   const commenter = getProfile(user);
   const { username, picture } = commenter;
   const navigate = useNavigate();
+  const [isCommentVisible, setIsCommentVisible] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const isCommentSelfPosted = username === 'carolibn';
   const isOwnPost = poster === 'carolibn';
 
@@ -31,52 +37,78 @@ export default function CommentCard({ poster, comment }) {
     navigate(`/profile?username=${username}`);
   };
 
+  const confirmDeleteComment = () => {
+    setTimeout(() => {
+      setIsCommentVisible(false);
+      onClose();
+    }, 500);
+  };
+
   return (
     <Flex align="center" w="full" my={2}>
-      <Flex justify="space-between" w="100%">
-        <Stack direction="row">
-          <Avatar size="sm" src={picture} mr={2} />
-          <Stack gap={0}>
-            <HStack w="full" gap={1}>
-              <Link color="black" onClick={handleUserNavigation}>
-                <Text as="b" fontSize="sm">
-                  {username}
+      {isCommentVisible ? (
+        <Flex justify="space-between" w="100%">
+          <Stack direction="row">
+            <Avatar size="sm" src={picture} mr={2} />
+            <Stack gap={0}>
+              <HStack w="full" gap={1}>
+                <Link color="black" onClick={handleUserNavigation}>
+                  <Text as="b" fontSize="sm">
+                    {username}
+                  </Text>
+                </Link>
+                <Text fontSize="sm" fontWeight={400}>
+                  {content}
                 </Text>
-              </Link>
-              <Text fontSize="sm" fontWeight={400}>
-                {content}
-              </Text>
-            </HStack>
-            <Tooltip
-              label={formatDate(postedAt)}
-              placement="bottom"
-              openDelay={500}
-            >
-              <Text fontSize="xs" color="gray" fontWeight={500}>
-                {formatDateDistanceToNow(postedAt)}
-              </Text>
-            </Tooltip>
+              </HStack>
+              <Tooltip
+                label={formatDate(postedAt)}
+                placement="bottom"
+                openDelay={500}
+              >
+                <Text fontSize="xs" color="gray" fontWeight={500}>
+                  {formatDateDistanceToNow(postedAt)}
+                </Text>
+              </Tooltip>
+            </Stack>
           </Stack>
-        </Stack>
-        {isCommentSelfPosted || isOwnPost ? (
-          <Menu>
-            <MenuButton
-              as={IconButton}
-              aria-label="Options"
-              icon={<FaEllipsis />}
-              variant="ghost"
-              color="gray"
-              size="xs"
-            />
-            <MenuList>
-              {isCommentSelfPosted ? (
-                <MenuItem icon={<EditIcon />}>Edit Comment</MenuItem>
-              ) : null}
-              <MenuItem icon={<DeleteIcon />}>Delete Comment</MenuItem>
-            </MenuList>
-          </Menu>
-        ) : null}
-      </Flex>
+          {isCommentSelfPosted || isOwnPost ? (
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<FaEllipsis />}
+                variant="ghost"
+                color="gray"
+                size="xs"
+              />
+              <MenuList>
+                {isCommentSelfPosted ? (
+                  <MenuItem icon={<EditIcon />} className={styles['menu-link']}>
+                    Edit Comment
+                  </MenuItem>
+                ) : null}
+                <MenuItem
+                  icon={<DeleteIcon />}
+                  onClick={onOpen}
+                  className={styles['menu-link']}
+                >
+                  Delete Comment
+                </MenuItem>
+                <ConfirmationModal
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  title="Delete this comment?"
+                  message="Are you sure you want to delete this comment? This action cannot be undone."
+                  buttonLabel="Delete"
+                  colorScheme="red"
+                  onConfirm={confirmDeleteComment}
+                />
+              </MenuList>
+            </Menu>
+          ) : null}
+        </Flex>
+      ) : null}
     </Flex>
   );
 }
