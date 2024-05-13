0920466841461ffd08217556331ad0c05239a896
Caroline Nguyen <carolbnguyen@gmail.com>
2024-05-13 07:32:14 -0700

    Add isLoading useState to ProfileHeader

--- client/src/components/ProfileHeader.jsx
+++ client/src/components/ProfileHeader.jsx
@@ -14,12 +14,15 @@ import { getCurrentUsername } from '../utils/userUtils';
 
 export default function ProfileHeader({ profile }) {
   const [isFollowing, setIsFollowing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [currentUsername, setCurrentUsername] = useState('');
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchCurrentUsername = async () => {
       const username = await getCurrentUsername();
       setCurrentUsername(username);
+      setIsLoading(false);
     };
     fetchCurrentUsername();
   }, []);
@@ -55,103 +58,107 @@ export default function ProfileHeader({ profile }) {
 
   return (
     <Flex mb={2}>
-      <Avatar size="xl" mr={5} src={picture} />
-      <Stack direction="column">
-        <Stack direction="row" gap={5}>
-          <Heading as="h1" size="md" noOfLines={1}>
-            {username}
-          </Heading>
-          {searchedUsername === currentUsername ? (
-            <Link to="/settings">
-              <Button colorScheme="blue" size="xs">
-                Edit Profile
-              </Button>
-            </Link>
-          ) : (
-            <Button
-              onClick={toggleIsFollowing}
-              colorScheme={isFollowing ? 'gray' : 'blue'}
-              size="xs"
-            >
-              {isFollowing ? 'Following' : 'Follow'}
-            </Button>
-          )}
-        </Stack>
-        <Stack direction="row" gap={8} mt={2}>
-          <Stack
-            direction={{ base: 'column', sm: 'row' }}
-            gap={{ base: 0, sm: 1 }}
-            align="center"
-          >
-            <Text as="b" fontSize="sm">
-              {photoCount}
-            </Text>
-            <Text fontSize="sm" fontWeight={500}>
-              {photoCount === 1 ? 'photo' : 'photos'}
+      {isLoading ? null : (
+        <>
+          <Avatar size="xl" mr={5} src={picture} />
+          <Stack direction="column">
+            <Stack direction="row" gap={5}>
+              <Heading as="h1" size="md" noOfLines={1}>
+                {username}
+              </Heading>
+              {searchedUsername === currentUsername ? (
+                <Link to="/settings">
+                  <Button colorScheme="blue" size="xs">
+                    Edit Profile
+                  </Button>
+                </Link>
+              ) : (
+                <Button
+                  onClick={toggleIsFollowing}
+                  colorScheme={isFollowing ? 'gray' : 'blue'}
+                  size="xs"
+                >
+                  {isFollowing ? 'Following' : 'Follow'}
+                </Button>
+              )}
+            </Stack>
+            <Stack direction="row" gap={8} mt={2}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                gap={{ base: 0, sm: 1 }}
+                align="center"
+              >
+                <Text as="b" fontSize="sm">
+                  {photoCount}
+                </Text>
+                <Text fontSize="sm" fontWeight={500}>
+                  {photoCount === 1 ? 'photo' : 'photos'}
+                </Text>
+              </Stack>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                gap={{ base: 0, sm: 1 }}
+                align="center"
+              >
+                <Text as="b" fontSize="sm">
+                  {statusCount}
+                </Text>
+                <Text fontSize="sm" fontWeight={500}>
+                  {statusCount === 1 ? 'status' : 'statuses'}
+                </Text>
+              </Stack>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                gap={{ base: 0, sm: 1 }}
+                align="center"
+                _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
+                onClick={onOpenFollower}
+              >
+                <Text as="b" fontSize="sm">
+                  {followersList && followersList.length}
+                </Text>
+                <Text fontSize="sm" fontWeight={500}>
+                  {followersList && followersList.length === 1
+                    ? 'follower'
+                    : 'followers'}
+                </Text>
+                <UserModal
+                  isOpen={isOpenFollower}
+                  onClose={onCloseFollower}
+                  title="Followers"
+                  usersList={followersList}
+                />
+              </Stack>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                gap={{ base: 0, sm: 1 }}
+                align="center"
+                _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
+                onClick={onOpenFollowing}
+              >
+                <Text as="b" fontSize="sm">
+                  {followingList && followingList.length}
+                </Text>
+                <Text fontSize="sm" fontWeight={500}>
+                  following
+                </Text>
+              </Stack>
+              <UserModal
+                isOpen={isOpenFollowing}
+                onClose={onCloseFollowing}
+                title="Following"
+                usersList={followingList}
+              />
+            </Stack>
+            <Heading as="h2" size="xs" noOfLines={1} my={2}>
+              {name}
+            </Heading>
+            <Text fontSize="sm" whiteSpace="pre-line">
+              {bio}
             </Text>
           </Stack>
-          <Stack
-            direction={{ base: 'column', sm: 'row' }}
-            gap={{ base: 0, sm: 1 }}
-            align="center"
-          >
-            <Text as="b" fontSize="sm">
-              {statusCount}
-            </Text>
-            <Text fontSize="sm" fontWeight={500}>
-              {statusCount === 1 ? 'status' : 'statuses'}
-            </Text>
-          </Stack>
-          <Stack
-            direction={{ base: 'column', sm: 'row' }}
-            gap={{ base: 0, sm: 1 }}
-            align="center"
-            _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
-            onClick={onOpenFollower}
-          >
-            <Text as="b" fontSize="sm">
-              {followersList && followersList.length}
-            </Text>
-            <Text fontSize="sm" fontWeight={500}>
-              {followersList && followersList.length === 1
-                ? 'follower'
-                : 'followers'}
-            </Text>
-            <UserModal
-              isOpen={isOpenFollower}
-              onClose={onCloseFollower}
-              title="Followers"
-              usersList={followersList}
-            />
-          </Stack>
-          <Stack
-            direction={{ base: 'column', sm: 'row' }}
-            gap={{ base: 0, sm: 1 }}
-            align="center"
-            _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
-            onClick={onOpenFollowing}
-          >
-            <Text as="b" fontSize="sm">
-              {followingList && followingList.length}
-            </Text>
-            <Text fontSize="sm" fontWeight={500}>
-              following
-            </Text>
-          </Stack>
-          <UserModal
-            isOpen={isOpenFollowing}
-            onClose={onCloseFollowing}
-            title="Following"
-            usersList={followingList}
-          />
-        </Stack>
-        <Heading as="h2" size="xs" noOfLines={1} my={2}>
-          {name}
-        </Heading>
-        <Text fontSize="sm" whiteSpace="pre-line">
-          {bio}
-        </Text>
-      </Stack>
+        </>
+      )}
     </Flex>
   );
 }
